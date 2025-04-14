caab60d311f12ee549d7f9a292f2d9e153a6d546
RDJ00 <rdj00@icloud.com>
2025-04-14 16:07:55 -0400

    Remove Sanity CMS files temporarily to fix build issues

--- lib/sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import { aboutPage } from './aboutPage'
-import { eventSchema } from './eventSchema'
-
-// Export all schemas for use in the Sanity config
-export const schemaTypes = [
-  aboutPage,
-  eventSchema,
-] 
